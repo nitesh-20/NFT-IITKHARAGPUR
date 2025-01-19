494337c23f20bf8a20bb12b5d6e9706fcab96fd7
nitesh-20 <nsahu6156@gmail.com>
2025-01-19 11:41:35 +0530

    NFT

--- Client/src/pages/NftExplorer.jsx
+++ Client/src/pages/NftExplorer.jsx
@@ -1,31 +1,88 @@
 import React from 'react';
+import './NftExplorer.css'; // Import the external CSS file
+import img1 from "../assets/img1.png";
+import img2 from "../assets/img2.jpeg";
+import img3 from "../assets/img3.jpeg";
+import img4 from "../assets/img4.jpeg";
+import img5 from "../assets/img5.png";
+import img6 from "../assets/img6.jpg";
+import img7 from "../assets/img7.jpg";
 
-const cardClass = "m-2 w-32 h-32 bg-card rounded-lg flex items-center justify-center";
-const imageUrl = "https://openui.fly.dev/openui/150x150.svg?text=";
-
-const NFTGallery = () => {
+const NftExplorer = () => {
   return (
-    <div className="flex flex-col items-center p-4">
-      <div className="flex flex-wrap justify-center">
-        <NFTCard alt="" text="Woodie+%235988" />
-        <NFTCard alt="CryptoSerg #358" text="CryptoSerg+%23358" />
-        <NFTCard alt="CryptoSerg #626" text="CryptoSerg+%23626" />
-        <NFTCard alt="Sneaky Vampire #8334" text="Sneaky+Vampire+%238334" />
-        <NFTCard alt="Zero [Genesis]" text="Zero+[Genesis]" />
-        <NFTCard alt="Yacht 2799" text="Yacht+2799" />
-        <NFTCard alt="NFT Image" text="nft_image" />
+    <div className="nft-explorer">
+      {/* Grid container for images */}
+      <div className="nft-grid">
+        <NFTCard 
+          alt="Woodie #5988" 
+          src={img1} 
+          name="Woodie #5988" 
+          price="2.5 ETH" 
+          profit={2100} 
+        />
+        <NFTCard 
+          alt="CryptoSerg #358" 
+          src={img2} 
+          name="CryptoSerg #358" 
+          price="3.0 ETH" 
+          profit={1800} 
+        />
+        <NFTCard 
+          alt="CryptoSerg #626" 
+          src={img3} 
+          name="CryptoSerg #626" 
+          price="4.0 ETH" 
+          profit={1750} 
+        />
+        <NFTCard 
+          alt="Sneaky Vampire #8334" 
+          src={img4} 
+          name="Sneaky Vampire #8334" 
+          price="5.2 ETH" 
+          profit={1500} 
+        />
+        <NFTCard 
+          alt="Zero [Genesis]" 
+          src={img5} 
+          name="Zero [Genesis]" 
+          price="6.0 ETH" 
+          profit={1430} 
+        />
+        <NFTCard 
+          alt="Yacht 2799" 
+          src={img6} 
+          name="Yacht 2799" 
+          price="7.5 ETH" 
+          profit={900} 
+        />
+        <NFTCard 
+          alt="Unknown NFT" 
+          src={img7} 
+          name="Unknown NFT" 
+          price="1.8 ETH" 
+          profit={526} 
+        />
       </div>
-      <div className="mt-4 text-lg font-semibold text-primary">0xaB...011a</div>
+      {/* Address text */}
+      <div className="nft-address">0xaB...011a</div>
     </div>
   );
 };
 
-const NFTCard = ({ alt, text }) => {
+const NFTCard = ({ alt, src, name, price, profit }) => {
   return (
-    <div className={cardClass}>
-      <img aria-hidden="true" alt={alt} src={`${imageUrl}${text}`} />
+    <div className="nft-card">
+      <img
+        aria-hidden="true"
+        alt={alt}
+        src={src}
+        className="nft-image"
+      />
+      <div className="nft-name">{name}</div>
+      <div className="nft-price">{`Price: ${price}`}</div>
+      <div className="nft-profit">{`+${profit}% Profit`}</div>
     </div>
   );
 };
 
-export default NFTGallery;
+export default NftExplorer;
